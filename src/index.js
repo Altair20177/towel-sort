d70627b28255fb415bf35c7740f70d56d336ff8e
Maksim <78382480+Altair20177@users.noreply.github.com>
2021-03-04 16:20:38 +0300

    reat: add new function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,38 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let duck = -1;
+let count = -1;
+let str1 = [];
+
+for (let j = 0; j < matrix.length; j++)
+{
+  let i = -1;
+  duck++;
+  if (duck %2 == 1)
+  {
+    do
+    {
+      i++;
+    }while(matrix[duck][i]);
+    do
+    {
+      i--;
+      count++;
+      str1[count] = matrix[duck][i];
+    }while(matrix[duck][i - 1]);
+  }
+  else 
+  {
+    do
+    {
+      count++;
+      i++;
+      str1[count] = matrix[duck][i];
+    }while(matrix[duck][i]);
+    str1.splice(count,1);
+    count--;
+  }
+} 
+return str1;
 }
